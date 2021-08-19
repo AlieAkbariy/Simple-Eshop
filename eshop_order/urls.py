5d7ff959bfb00422c2da1366a623e09a663f501f
Ali <alieakbariy@gmail.com>
2021-08-19 19:20:41 +0430

    connect Eshop to zarinpal

--- eshop_order/urls.py
+++ eshop_order/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from eshop_order.views import add_order_user, open_order
+from eshop_order.views import add_order_user, open_order, send_request, verify
 
 urlpatterns = [
     path('add-order-user', add_order_user),
-    path('open-order', open_order)
+    path('open-order', open_order),
+    path('request', send_request, name='request'),
+    path('verify', verify, name='verify'),
 ]
